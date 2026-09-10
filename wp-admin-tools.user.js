09f3b64a2bac3e0073e02596c5e1f55111add77f
Toha <black.owl0192@gmail.com>
2026-09-10 20:20:10 +0300

    Bump version to 1.9.16

--- wp-admin-tools.user.js
+++ wp-admin-tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        WP Admin Панель для ЧБ
 // @namespace    https://github.com/blackowl0192/script_folder
-// @version      1.9.15
+// @version      1.9.16
 // @description  Единая панель для WP Admin: добавление юзера в БД, редактирование ордера ЧБ, редактирование ЛОГ
 // @author       Black Owl
 // @match        *://*/wp-admin/*
